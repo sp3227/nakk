220be013d5a64ae973e357858c27a42e45393192
sejung <sp3227@gmail.com>
2017-02-12 14:25:59 +0900

    2017/02/12  home

---
+++ app/src/main/java/com/moduse/nakk/TalkData.java
@@ -0,0 +1,19 @@
+package com.moduse.nakk;
+
+/**
+ * Created by sejung on 2017-02-11.
+ */
+
+public class TalkData
+{
+    private String idx;
+    private String talk_writeid;
+    private String talk_img;
+    private String talk_data;
+    private String talk_likecount;
+    private String talk_mentcount;
+    private String talk_locationstate;
+    private String talk_latitude;
+    private String talk_longitude;
+    private String talk_writetime;
+}
