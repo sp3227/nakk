748f2ae1f8f7162f3f265a5d382f14284e5f7924
sejung <sp3227@gmail.com>
2017-02-18 11:38:19 +0900

    sdfsdf

---
+++ app/src/main/java/com/moduse/nakk/Tab1_map.java
@@ -0,0 +1,22 @@
+package com.moduse.nakk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HOMESJ on 2017-02-18.
+ */
+
+public class Tab1_map extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab1_map);
+
+
+
+    }
+}
